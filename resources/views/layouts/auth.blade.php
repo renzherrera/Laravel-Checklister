7b371b6ec17e0affd835e0586be987da2f79fdb9
unknown <renzgabrielherrera1@gmail.com>
2021-06-02 16:49:11 +0800

    Updated laravel checklister

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,88 +7,23 @@
 
  <!-- CoreUI CSS -->
  <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css" crossorigin="anonymous">
- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/css/perfect-scrollbar.min.css" />
 
  <title>CoreUI</title>
  </head>
- <body class="c-app">
-    @include('partials.sidebar')
 
-  <div class="c-wrapper c-fixed-components">
-    <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
-      <button class="c-header-toggler c-class-toggler d-lg-none mfe-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show">
-        <svg class="c-icon c-icon-lg">
-          <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-menu')}}"></use>
-        </svg>
-      </button><a class="c-header-brand d-lg-none" href="#">
-        <svg width="118" height="46" alt="CoreUI Logo">
-          <use xlink:href="assets/brand/coreui.svg#full"></use>
-        </svg></a>
-      <button class="c-header-toggler c-class-toggler mfs-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true">
-        <svg class="c-icon c-icon-lg">
-          <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-menu')}}"></use>
-        </svg>
-      </button>
-    
-      <ul class="c-header-nav ml-auto mr-4">
-        <li class="c-header-nav-item d-md-down-none mx-2"><a class="c-header-nav-link" href="#">
-            <svg class="c-icon">
-              <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-bell')}}"></use>
-            </svg></a></li>
-        <li class="c-header-nav-item d-md-down-none mx-2"><a class="c-header-nav-link" href="#">
-            <svg class="c-icon">
-              <use xlink:href="{{asset('vendors/@coreui/icons/svg/free.svg#cil-list-rich')}}"></use>
-            </svg></a></li>
-        <li class="c-header-nav-item d-md-down-none mx-2"><a class="c-header-nav-link" href="#">
-            <svg class="c-icon">
-              <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
-            </svg></a></li>
-        <li class="c-header-nav-item dropdown"><a class="c-header-nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-            <div class="c-avatar"><img class="c-avatar-img" src="assets/img/avatars/6.jpg" alt="user@email.com"></div>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right pt-0">
-            <div class="dropdown-header bg-light py-2"><strong>Account</strong></div><a class="dropdown-item" href="#">
-              <svg class="c-icon mr-2">
-                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
-              </svg> Updates<span class="badge badge-info ml-auto">42</span></a>
-            <div class="dropdown-header bg-light py-2"><strong>Settings</strong></div><a class="dropdown-item" href="#">
-              <svg class="c-icon mr-2">
-                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
-              </svg> Profile</a><a class="dropdown-item" href="#">
-              <svg class="c-icon mr-2">
-                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-settings"></use>
-              </svg> Settings</a><a class="dropdown-item" href="#">
-              <svg class="c-icon mr-2">
-                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-credit-card"></use>
-              </svg> Payments<span class="badge badge-secondary ml-auto">42</span></a>
-            <div class="dropdown-divider"></div><a class="dropdown-item" href="#">
-              <svg class="c-icon mr-2">
-                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
-              </svg> Lock Account</a>
-              <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();  document.getElementById('logout-form').submit();">
-              <svg class="c-icon mr-2">
-                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-account-logout"></use>
-              </svg> {{ __('Logout') }}
-            </a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-              @csrf
-            </form>
-
-
-
-         
-
-
-          </div>
-        </li>
-      </ul>
-      
-    </header>
-    <div class="c-body">
-        <main class="c-main">
-            @yield('content')
-        </main>
-
+ <body class="c-app flex-row align-items-center" >
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card-group">
+                        <div class="card-body">
+                            @yield('content')
+                        </div>
+                     </div>
+            </div>
+        </div>
+    </div>
+ 
  <!-- Optional JavaScript -->
  <!-- Popper.js first, then CoreUI JS -->
  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.0/perfect-scrollbar.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +25,9 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware'=> 'auth'], function(){
+    Route::group(['prefix' => 'admin', 'as'=> 'admin.', 'middleware' => 'is_admin'],function(){
+    Route::resource('pages', PageController::class);
+    });
+});
